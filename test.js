eb06348198d9e9f3e12bc9599891cb04569a1f76
danoseun <ayodejisomefun@yahoo.com>
2019-08-19 11:24:36 +0100

    update heroku

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 import { inputValidation, deleteAttribute, optimalFunction } from './api/api'
 import app from './app';
 
+const { expect } = chai
+
+chai.use(chaiHttp);
+
 describe('Test functions', () => {
     it('DeleteAttribute Should return other properties without deleted property', () => {
-        expect(deleteAttribute({ type: 'durban', crux: 'Indices', color: 'green', title: 'Indict the idiot' }, 'type').to.equal({ crux: 'Indices', color: 'green', title: 'Indict the idiot' }));
+        expect(deleteAttribute({ type: 'durban', crux: 'Indices', color: 'green', title: 'Indict the idiot' }, 'type').to.include({ crux: 'Indices', color: 'green', title: 'Indict the idiot' }));
     });
 
-    it('optimalFunction Should index', () => {
+    it('optimalFunction Should return index', () => {
         expect(deleteAttribute(optimalFunction([3,2,5,4], [2,3,4,2])).to.equal(0));
     });
 
